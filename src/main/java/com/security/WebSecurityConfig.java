8d03b6ca979cde91f4be2cfa33b548ae79737fa0
Cedric Arnhold <unicedric@gmail.com>
2024-06-10 13:45:19 +0200

    testen ob die Post und Get Anfragen durchlaufen

--- src/main/java/com/security/WebSecurityConfig.java
+++ src/main/java/com/security/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -63,6 +64,11 @@ public class WebSecurityConfig {
                     .allowedHeaders("*")
                     .allowCredentials(true);
         }
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
     }
 
     @Bean
