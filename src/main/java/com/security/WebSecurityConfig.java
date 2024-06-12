ebab12f1a9e7173aaa3bb97700fec41a98defcc0
Cedric Arnhold <unicedric@gmail.com>
2024-06-12 13:23:43 +0200

    http://localhost:8080 entfernt

--- src/main/java/com/security/WebSecurityConfig.java
+++ src/main/java/com/security/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig {
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/api/**")
                     .allowedOrigins(
-                            "http://localhost:5173", "http://localhost:8080",
+                            "http://localhost:5173",
                             "https://virtual-pet-bcky.onrender.com")
                     .allowedMethods("*")
                     .allowedHeaders("*")
